a86704352e95d687fd2353e14da6b50eed63faa8
castmchen <chenqiang_590@163.com>
2019-01-14 11:17:16 +0800

    commit test websocket

--- bin/www.js
+++ bin/www.js
@@ -5,11 +5,32 @@
 const server = require("../dist/server.js")
 const debug = require("debug")("express:server")
 const http = require("http")
+const webSocket = require("ws")
 const httpPort = normalizePort(process.env.Port || 8080)
 const app = server.Server.bootstrap().app
-const httpServer = http.createServer(app).listen(httpPort)
+const httpServer = http.createServer(app)
 httpServer.on("error", onError)
 httpServer.on("listening", onListening)
+const wss = new webSocket.Server({ noServer: true })
+wss.on("connection", (ws) => {
+    ws.on("message", (message) => {
+        debug(message);
+    })
+    ws.send("you are right")
+})
+httpServer.on('upgrade', (request, socket, head) => {
+    const pathname = url.parse(req.url).pathname;
+
+    if (pathname === '/chat') {
+        wss.handleUpgrade(request, socket, head, (ws) => {
+            debug("server has been connect successfully by wss")
+            wss.emit("connection", ws, request)
+        })
+    } else {
+        socket.destroy()
+    }
+})
+httpServer.listen(httpPort)
 
 
 function normalizePort(val) {
